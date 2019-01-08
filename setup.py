8ae37b80e581e2a0c3e6bd35f0d87fa0e672adc9
Ethan Payne <epay1@student.monash.edu>
2019-01-08 16:03:58 +1100

    still boilerplate stuff

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.0.1',
     author='E. Payne',
     packages=find_packages(),
-    scripts=['bin/testing.py'],
     license='LICENSE.txt',
     description='Code for the analysis of BNS merger \
      signals in GW and EM bands',
